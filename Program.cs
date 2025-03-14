23f94446da150fbe6bd51a677829f73509f54e6d
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-03-14 10:19:13 +0900

    #2 사칙연산 후 출력

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace Project02_1
     {
         static void Main(string[] args)
         {
+            int a, b;
+            // 나중에 입력 받을 예정
+
+            int result;
+
+            a = 100;
+            b = 50;
+
+            result = a + b;
+            Console.WriteLine(a + " + " + b + " = " + result); // operator
+
+            result = a - b;
+            Console.WriteLine(a + " - " + b + " = " + result); // operator
+
+            result = a * b;
+            Console.WriteLine(a + " * " + b + " = " + result); // operator
+
+            result = a / b;
+            Console.WriteLine(a + " / " + b + " = " + result); // operator
         }
     }
 }
